7a1c7c3324ea90a8ea9af80e736ba4596529c262
S184929 <sigurd.staloy@gmail.com>
2025-01-23 15:19:51 +0100

    Update FilmarkivTest.java

---
+++ src/no/hvl/data102/filmarkiv/test/FilmarkivTest.java
@@ -0,0 +1,30 @@
+package no.hvl.data102.filmarkiv.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+import impl.Film;
+import impl.Filmarkiv;
+import impl.Sjanger;
+import no.hvl.data102.filmarkiv.impl.Filmarkiv;
+import no.hvl.data102.filmarkiv.impl.Film;
+import no.hvl.data102.filmarkiv.impl.Sjanger;
+
+class FilmarkivTest {
+	
+	private Filmarkiv arkiv;
+	
+	void setup() throws Exception {
+		arkiv = new Filmarkiv(5);
+		
+		
+	}
+	@Test
+	void testLeggTilOgFjernFilm() {
+		Film film1 = new Film("Produsent1" , "Tittel1", 2020, "Filmselskap1", Sjanger.ACTION);
+		arkiv.leggTilFilm(film1);
+		assertEquals(1, arkiv.antall(), "Antall filmer bør være 1 etter å ha lagt til en film.");
+	}
+
+}
