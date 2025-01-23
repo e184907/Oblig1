0cd7e9fdcc663520148d3da128a93f14a4354cd6
b184896 <bredetorgersen@gmail.com>
2025-01-23 14:35:25 +0100

    push

---
+++ src/no/hvl/data102/filmarkiv/test/ivh.java
@@ -0,0 +1,14 @@
+package no.hvl.data102.filmarkiv.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ivh {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
